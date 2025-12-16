efc6dd39f381dbaad83ca39f9ec612e11382311a
Danny Shen <shenfive@gmail.com>
2025-12-16 17:44:02 +0800

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Danny Shen on 2025/12/16.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
