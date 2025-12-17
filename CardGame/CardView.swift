b6b9fbeeaac8d43a00a76dad7733152d8d6ee3b3
Danny Shen <shenfive@gmail.com>
2025-12-17 10:32:29 +0800

    新增減圖片數量

--- CardGame/CardView.swift
+++ CardGame/CardView.swift
@@ -11,29 +11,21 @@ import SwiftUI
 struct CardView: View {
 
     let emoji: String
-//    let width: CGFloat
 
     @State private var isFaceUp = true
     @State private var rotation: Double = 0
-
-//    private var height: CGFloat {
-//        width * 4.0 / 3.0
-//    }
 
     var body: some View {
         ZStack {
             // 正面
             frontView
                 .opacity(isFaceUp ? 1 : 0)
-
             // 背面（預先旋轉 180°，避免文字倒轉）
             backView
                 .opacity(isFaceUp ? 0 : 1)
                 .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
         }
-//        .frame(width: width, height: height)
-        .rotation3DEffect(.degrees(rotation),
-                          axis: (x: 0, y: 1, z: 0))
+        .rotation3DEffect(.degrees(rotation),axis: (x: 0, y: 1, z: 0))
         .onTapGesture {
             flipCard()
         }
@@ -41,14 +33,10 @@ struct CardView: View {
 
     private var frontView: some View {
         let shape = RoundedRectangle(cornerRadius: 12)
-
         return ZStack {
-            shape
-                .fill(Color.white)
-            shape
-                .stroke(Color.red, lineWidth: 4)
-            Text(emoji)
-               
+            shape.fill(Color.white)
+            shape.stroke(Color.red, lineWidth: 2)
+            Text(emoji).font(.largeTitle)
         }
     }
 
@@ -61,7 +49,6 @@ struct CardView: View {
         withAnimation(.easeInOut(duration: 0.4)) {
             rotation += 180
         }
-
         // 在轉到一半時切換正反面狀態
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
             isFaceUp.toggle()
--- CardGame/ContentView.swift
+++ CardGame/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
 
+    // 定義一組 emoji 候選字元
+    let emojis = ["😀","😅","😂","😍","😎","🤩","🥳","😴","🤯","😇", "🤓","🧐","😡","😭","😱","🤔","🙄","😤","😷","🤒", "👻","💩","👽","🤖","🎃","🐶","🐱","🐭","🐹","🐰", "🦊","🐻","🐼","🐨","🐯","🦁","🐮","🐷","🐸","🐵", "🐔","🐧","🐦","🐤","🐣","🐥","🦆","🦅","🦉","🦇", "🐺","🐗","🐴","🦄","🐝","🐛","🦋","🐌","🐞","🐜"].shuffled()
+
 //    let columns = [
 //        GridItem(.flexible()),       // 平均分配
 //        GridItem(.flexible()),       // 平均分配
@@ -17,18 +20,43 @@ struct ContentView: View {
 //        GridItem(.flexible())       // 平均分配
 //    ]
     let columns = Array(repeating: GridItem(.flexible()), count: 5)
-    var numberOfCards = 100
+
+    @State var numberOfCards = 20
     var body: some View {
-        ScrollView{
-            LazyVGrid(columns: columns,spacing: 0) {
-                ForEach(0..<numberOfCards,id:\.self){ index in
-                    CardView(emoji: "😍")
-                        .aspectRatio(3/4, contentMode: .fit) // 維持 4:3 直立比例
-                        .frame(maxWidth: .infinity)          // 吃滿欄位寬
-                        .padding(5)
+        VStack{
+            ScrollView{
+                LazyVGrid(columns: columns,spacing: 0) {
+                    ForEach(0..<numberOfCards,id:\.self){ index in
+                        CardView(emoji: emojis[index])
+                            .aspectRatio(3/4, contentMode: .fit) // 維持 4:3 直立比例
+                            .frame(maxWidth: .infinity)          // 吃滿欄位寬
+                            .padding(4)
+                    }
                 }
-            }.padding()
+            }
+            HStack{
+                Button(action: {
+                    if numberOfCards < 50{
+                        numberOfCards += 1
+                    }
+                }) {
+                    Image(systemName: "plus.circle")
+                        .font(.title)
+                }
+                Spacer()
+                Button(action: {
+                    if numberOfCards > 1 {
+                        numberOfCards -= 1
+                    }
+                }) {
+                    Image(systemName: "minus.circle")
+                        .font(.title)
+                }
+
+            }
         }
+        .padding()
+//        .animation(.easeInOut,value: numberOfCards)
     }
 }
 
